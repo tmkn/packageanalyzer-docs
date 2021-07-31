4dc92156eab9dc5961a9e800671b4a292a197af6
tmkn <tuomasklein@gmail.com>
2021-08-01 00:17:24 +0200

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/tmkn/packageanalyzer-docs/edit/master/blog/',
+            'https://github.com/tmkn/packageanalyzer-docs/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
